feb2e7b20cdf3abb8b32a82ae18f109802e3fc42
andrey-trasov <159887936+andrey-trasov@users.noreply.github.com>
2024-07-19 13:14:46 +0300

    задание 1.1

--- mailings/urls.py
+++ mailings/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from mailings.apps import MailingsConfig
 from mailings.views import ClientListView, ClientCreateView, ClientDetailView, ClientUpdateView, ClientDeleteView, \
-    MessageListView, MessageCreateView, MessageUpdateView, MessageDeleteView, MessageDetailView
+    MessageListView, MessageCreateView, MessageUpdateView, MessageDeleteView, MessageDetailView, NewsletterListView, \
+    NewsletterCreateView, NewsletterUpdateView, NewsletterDeleteView, NewsletterDetailView
 
 app_name = MailingsConfig.name
 urlpatterns = [
@@ -14,7 +15,12 @@ urlpatterns = [
     path('message_create/', MessageCreateView.as_view(), name='message_create'),
     path('message_update/<int:pk>/',MessageUpdateView.as_view(), name='message_update'),
     path('message_delete/<int:pk>/', MessageDeleteView.as_view(), name='message_delete'),
-    path('message/<int:pk>/', MessageDetailView.as_view(), name='message_detail')
+    path('message/<int:pk>/', MessageDetailView.as_view(), name='message_detail'),
+    path('newsletter_list/', NewsletterListView.as_view(), name='newsletter_list'),
+    path('newsletter_create/', NewsletterCreateView.as_view(), name='newsletter_create'),
+    path('newsletter_update/<int:pk>/',NewsletterUpdateView.as_view(), name='newsletter_update'),
+    path('newsletter_delete/<int:pk>/', NewsletterDeleteView.as_view(), name='newsletter_delete'),
+    path('newsletter/<int:pk>/', NewsletterDetailView.as_view(), name='newsletter_detail')
 ]
 
 
